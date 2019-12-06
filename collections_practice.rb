674365f51f4cab4cd01aa008ed0700cf28fbf6f5
Michael Fahim <michael_fahim@yahoo.com>
2019-12-06 16:24:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,60 @@
 def sort_array_asc(array)
- 
-  array_sorted << array.sort
+  array.sort
 end
+
+sort_array_asc([25, 7, 1]) #[1,7,25]
+
+def sort_array_desc(array)
+  array.sort do | left, right|
+    right <=> left
+  end
+end
+
+sort_array_desc([25, 7, 14]) #[25, 14, 7]
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+sort_array_char_count(["dogs", "cat", "Horses"]) #["cat", "dogs", "Horses"]
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+swap_elements(["blake", "ashley", "scott"]) #["blake", "scott", "ashley"]
+
+def reverse_array(array)
+  array.reverse
+end
+
+reverse_array(["blake", "ashley", "scott"]) #["scott", "ashley", "blake"]
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+kesha_maker(["blake", "ashley", "scott"]) #["bl$ke", "as$ley", "sc$tt"]
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+
+
+end
+
+find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) #["apple", "avis", "arlo", "ascot"]
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+end 
