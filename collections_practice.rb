b095984e032cf9930dfafb3d6de9c6418e8552a9
Michael Fahim <michael_fahim@yahoo.com>
2019-12-06 16:33:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,3 +58,18 @@ def sum_array(array)
   end
   sum
 end 
+end
+
+sum_array([11,4,7,8,9,100,134]) #273
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
+
+add_s(["hand","feet", "knee", "table"])
